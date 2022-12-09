0922b21ccf1dc11c2b6655393ad1b18f63d4ab96
Nahuelement <nahuel.perugi@gmail.com>
2022-12-08 21:45:01 -0300

    update

--- app/app/settings.py
+++ app/app/settings.py
@@ -157,7 +157,7 @@ SPECTACULAR_SETTINGS = {
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD ='dmhhwztafdmrswqo'#str( os.environ.get('EMAIL_HOST_PASSWORD'))
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 print(os.environ.get('EMAIL_HOST_USER'))
