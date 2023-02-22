6d563cce8a348e438fb836d7fd9b02ebdae4dbc7
Nahuelement <nahuel.perugi@gmail.com>
2023-02-21 22:33:53 -0300

    push

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,7 +5,7 @@ set -e
 python manage.py wait_for_db
 python manage.py collectstatic --noinput
 python manage.py migrate
-sleep 5 
+sleep 10
 python subscriber.py &
 
 uwsgi --socket :9000 --workers 4 --master --enable-threads --module app.wsgi
